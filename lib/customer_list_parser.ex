83c18c68aa19b43605d0024d80e6555c2aedd92a
Igor Artemenko <igor@codemonium.com>
2021-07-16 23:12:54 -0700

    Initial commit

---
+++ lib/customer_list_parser.ex
@@ -0,0 +1,18 @@
+defmodule CustomerListParser do
+  @moduledoc """
+  Documentation for CustomerListParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CustomerListParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/customer_list_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule CustomerListParserTest do
+  use ExUnit.Case
+  doctest CustomerListParser
+
+  test "greets the world" do
+    assert CustomerListParser.hello() == :world
+  end
+end
