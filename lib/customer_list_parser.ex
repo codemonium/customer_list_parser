0a26f4bf4bc13587be198fb0d6b677353f575dc7
Igor Artemenko <igor@codemonium.com>
2021-07-17 08:51:14 -0700

    Implement function for extracting App User IDs

--- lib/customer_list_parser.ex
+++ lib/customer_list_parser.ex
@@ -1,18 +1,60 @@
 defmodule CustomerListParser do
   @moduledoc """
-  Documentation for CustomerListParser.
+  Functions for parsing customer lists exported by the [RevenueCat](https://www.revenuecat.com/) service.
+
+  The exported customer lists are gzipped CSV files.
+  For security reasons, customers can only be identified by a so-called App User ID.
+  Each App User ID is a UUID associated to some record stored outside of RevenueCat.
+  This tool extracts these UUIDs.
   """
 
   @doc """
-  Hello world.
+  Extracts all (non-anonymous) App User IDs from the given (gzipped) CSV file.
+
+  Note: the CSV files always use a semi-colon as the separator.
+  """
+  def extract_app_user_ids(path_to_file) when is_binary(path_to_file) do
+    {:ok, pid} = path_to_file |> File.read!() |> :zlib.gunzip() |> StringIO.open()
+
+    app_user_ids =
+      pid
+      |> IO.binstream(:line)
+      |> CSV.decode(separator: ?;, headers: true)
+      |> Enum.filter(fn {result, row} ->
+        if result !== :ok do
+          IO.puts(:stderr, row)
+        end
+
+        result === :ok
+      end)
+      |> Enum.map(fn {:ok, row} -> row["app_user_id"] end)
+      |> Enum.filter(fn app_user_id -> not String.starts_with?(app_user_id, "$RCAnonymousID") end)
+
+    count = app_user_ids |> Enum.count()
+    uniq_count = app_user_ids |> Enum.uniq() |> Enum.count()
+    duplicates = app_user_ids -- Enum.uniq(app_user_ids)
+
+    IO.puts(:stderr, "Extracted #{count} (#{uniq_count} unique) App User IDs")
+
+    if not Enum.empty?(duplicates) do
+      IO.puts(:stderr, "Duplicate App User IDs: #{duplicates}")
+    end
+
+    app_user_ids
+  end
+
+  @doc ~S"""
+  Generates a string that serves as a value in a SQL `WHERE` `IN` clause.
 
   ## Examples
 
-      iex> CustomerListParser.hello()
-      :world
-
+      iex> CustomerListParser.generate_where_clause(["Apple", "Banana", "Carrot"])
+      "(\n'Apple',\n'Banana',\n'Carrot'\n)"
   """
-  def hello do
-    :world
+  def generate_where_clause(app_user_ids) when is_list(app_user_ids) do
+    string =
+      app_user_ids |> Enum.map(fn app_user_id -> "'#{app_user_id}'" end) |> Enum.join(",\n")
+
+    "(\n#{string}\n)"
   end
 end
--- test/customer_list_parser_test.exs
+++ test/customer_list_parser_test.exs
@@ -1,8 +1,4 @@
 defmodule CustomerListParserTest do
   use ExUnit.Case
   doctest CustomerListParser
-
-  test "greets the world" do
-    assert CustomerListParser.hello() == :world
-  end
 end
